b1f0917db2190cd7e8dd9b423f5ce8dd17c6e0a6
carolcao2020 <31724399+carolcao2020@users.noreply.github.com>
2018-11-14 17:29:24 +0800

    pipeline job script

---
+++ src/pipeline_job.groovy
@@ -0,0 +1,25 @@
+import hudson.model.*;
+
+println env.JOB_NAME
+println env.BUILD_NUMBER
+println env.WORKSPACE
+
+pipeline{
+    agent any
+    stages{
+        stage('init'){
+            steps{
+                script{
+                    model_test = load env.WORKSPACE + "/pipeline/module/pipeline-demo-module.groovy"
+                }
+            }
+        }
+        stage('Test Method'){
+            steps{
+                script{
+                    log_files = model_test.find_files('**/*.log')
+                }
+            }
+        }
+    }
+}
