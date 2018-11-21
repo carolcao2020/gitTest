2384aeb02643a05ab9e7f6ef58aa7cb4b2866b39
carolcao2020 <31724399+carolcao2020@users.noreply.github.com>
2018-11-21 16:13:55 +0800

    add read_yaml_file method

--- pipeline/module/pipeline-demo-module.groovy
+++ pipeline/module/pipeline-demo-module.groovy
@@ -41,4 +41,17 @@ def read_properties(properties_file){
         println (it.key + " = " + it.value)
     }
 }
+
+def read_yaml_file(yaml_file){
+    def datas = ""
+    if(yaml_file.toString().endsWith(".yml")){
+        datas = readYaml file : yaml_file
+    }
+    else{
+        datas = readYaml text: yaml_file
+    }
+    datas.each{
+        println (it.key + " = " + it.value)
+    }
+}
 return this;
